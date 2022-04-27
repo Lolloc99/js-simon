f6c4d2b1a8287cfc332d7129166165466cef0378
Lolloc99 <lolloc99@yahoo.it>
2022-04-27 17:06:02 +0200

    Esercizio base concluso, tasto restart in lavorazione

--- js/script.js
+++ js/script.js
@@ -9,12 +9,13 @@ Consigli del giorno:
 * Individuate gli elementi di cui avete bisogno per realizzare il programma.
 */
 
+const maxGuessNumber = 5;
 // Creare un array di 5 numeri casuali unici
-const rndArray = rndNumberGenerator(5, 50);
+let rndArray = rndNumberGenerator(maxGuessNumber, 100);
 // Imposto il tempo del timer (in secondi)
 const timeSec = 3;
 // Creo l'array vuoto dei numeri dell'utente
-const userArray = [];
+let userArray = [];
 
 // Estraggo i bottoni con l'id e li affido ad una variabile per comodità
 const startBtn = document.getElementById("start-btn");
@@ -32,30 +33,45 @@ startBtn.addEventListener("click",
 
         // Avviare il timer e al suo termine, chiedere all'utente di inserire i numeri visti, uno alla volta tramite prompt, ed inseririli dentro "userArray"
         const timer = setTimeout(timerFunction, (timeSec) * 1000);
+        const timerHidden = setTimeout(timerHiddenFunction, (timeSec - 1) * 1000);
+
+        function timerHiddenFunction() {
+            numString.classList.add("hidden");
+        }
+
         function timerFunction() {
-            //numString.classList.add("hidden"); // I numeri scompagliono solo dopo l'inserimento dei 5 nume *ticket*
             for (let i = 1; i <= 5; i++) {
                 let digitNum = parseInt(prompt(`${"Inserisci il numero"} ${i} ${"visualizzato:"}`));
                 userArray.push(digitNum);
                 console.log(userArray);
                 console.log(rndArray);
             }
+            
+            // Fare un ciclo che analizza i numeri interni all'array random e quelli interni all'array dell'utente, confrontandoli e segnalando se elemRndArray === elemUserArray, dicendo n. e oggetto di quelli ambi presenti
+            let result = similaritiesExaminator(rndArray, userArray);
+            console.log("I numeri simili sono:", result);
+            const resultString = document.getElementById("simonResult");
+            resultString.innerHTML = `${"I numeri il cui valore e la cui posizione sono state indovinate sono:"} ${result}`;
+            const subResultString = document.getElementById("simonResultSub");
+            subResultString.innerHTML = `${"ovvero"} ${result.length} ${"su"} ${maxGuessNumber}`;
         }
             
-    });
+    }
+);
 
-// Fare un ciclo che analizza i numeri interni all'array random e quelli interni all'array dell'utente, confrontandoli e segnalando se elemRndArray === elemUserArray, dicendo n. e oggetto di quelli ===
-
-//function similaritiesExaminator(rndArray, userArray)
-
-
-//Restart button
+// Restart button
 restartBtn.addEventListener("click",
     function () {
 
         startBtn.style.pointerEvents = "initial";
-        //svuota ogni array
-    });
+        userArray = [];
+        rndArray = [];
+        
+
+    }
+);
+
+
 
 // FUNCTIONS
 /*
@@ -93,14 +109,15 @@ function getRndInteger(min, max) {
 Descrizione: controlla che se il contenuto di un'array sia presente anche in un secondo array
     Dato 1: Array 1 → "firstArray"
     Dato 2: Array 2 → "secondArray"
-    Return: bo
+    Return: Array dei numeri uguali nella stessa posizione
 */
-
 function similaritiesExaminator(firstArray, secondArray) {
+    const SimiliarNumberArray = [];
     for (let i = 0; i < firstArray.length; i++) {
         if (firstArray[i] === secondArray[i]) {
-            
+            SimiliarNumberArray.push(secondArray[i])
         }
-        
     }
+
+    return SimiliarNumberArray
 }
